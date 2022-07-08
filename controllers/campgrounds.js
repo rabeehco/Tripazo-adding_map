8baa68c1fa0a1ccbe8099f085f1df7773833cceb
codewithrabeeh <real.ray48@gmail.com>
2022-07-08 11:39:39 +0530

    working with GeoJSON

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -21,17 +21,14 @@ module.exports.createCampground = async (req, res, next) => {
         query: req.body.campground.location,
         limit: 1
     }).send()
-    res.send(geoData.body.features[0].geometry.coordinates)
-    // res.send('OK!!!!')
-    // const campground = new Campground(req.body.campground)
-    // campground.images = req.files.map(f => ({url: f.path, filename: f.filename}));
-    // campground.author = req.user._id;
-    // await campground.save()
-    // req.flash('success', 'Successfully made a new campground')
-    // res.redirect(`/campgrounds/${
-    //     campground._id
-    // }`)
-    // next(e)
+    const campground = new Campground(req.body.campground)
+    campground.geometry = geoData.body.features[0].geometry
+    campground.images = req.files.map(f => ({url: f.path, filename: f.filename}));
+    campground.author = req.user._id;
+    await campground.save()
+    console.log(campground) 
+    req.flash('success', 'Successfully made a new campground')
+    res.redirect(`/campgrounds/${campground._id}`)
 }
 
 /* Show a Campground Page */
