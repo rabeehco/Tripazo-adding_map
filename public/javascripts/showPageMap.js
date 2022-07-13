f78028e03f407ebf4de48bb76144a28d9eb3c837
codewithrabeeh <real.ray48@gmail.com>
2022-07-13 18:45:16 +0530

    displaying a map part 1.1

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,8 +1,12 @@
 
-    mapboxgl.accessToken = mapToken;
-    const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [-74.5, 40],
-    zoom: 9
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+container: 'map', // container ID
+style: 'mapbox://styles/mapbox/streets-v11', // style URL
+center: [-74.5, 40], // starting position [lng, lat]
+zoom: 9, // starting zoom
+projection: 'globe' // display the map as a 3D globe
+});
+map.on('style.load', () => {
+map.setFog({}); // Set the default atmosphere style
 });
